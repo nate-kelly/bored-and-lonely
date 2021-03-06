a5be675b66c3abd1fe6e33ecb7f231fe6fc77933
ancientmelody <ndavidkelly@gmail.com>
2021-03-06 14:35:50 -0500

    created form component and handler functions

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import './App.css';
+// import './App.css';
+import Form from './Form.js';
 import axios from 'axios';
 import { useState } from 'react';
 
 function App() {
-  // Make a call to the Bored API
-  const handleClick = () => {
+  // Make a call to the API on form submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
     axios({
       method: 'GET',
-      url: 'http://www.boredapi.com/api/activity/',
+      url: `http://www.boredapi.com/api/activity?type=${type}`,
       dataResponse: 'JSON',
       params: {
         format: 'JSON'
       }
     }).then(response => {
-      console.log(response.data.activity);
       setActivity(response.data.activity);
     })
+  }
+  
+  const handleChange = (radioValue) => {
+    setType(radioValue);
   }
 
   // Store results of that call in state
   const [ activity, setActivity ] = useState('');
-  console.log(useState());
-
-  // Display results on the page based on user input
-
-  // Questions
-    // Do I need useEffect if just calling the API on click? Nope.
-    // Should I store API call in a separate component? Nope.
-    // Create one for each section of the page? For the results?
-      // Form can be its own component, passing function using props to get user's selection
-      // Displaying activities (results) can be its own component
+  const [ type, setType ] = useState('');
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Project 3 placholder title</h1>
-        <button onClick={handleClick}>Call the API</button>
+        <h1>Bored &amp; Lonely</h1>
+        <p>An activity generator for lethargic lockdowns</p>
+        <Form showResults={handleSubmit} handleChange={handleChange}/>
         <p>{activity}</p>
       </header>
     </div>
@@ -43,3 +40,6 @@ function App() {
 }
 
 export default App;
+
+// To Do:
+  // Create activities component and display results on page based on user selection
